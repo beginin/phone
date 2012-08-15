3218497eb692d0969550ae4ac0fe03740b10137c
Vladimir Beginin <beginin@mail.ru>
2012-08-15 13:20:12 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phone::Application.config.secret_token = '6837a430763ba571e96d3bcc96f3bd04523b2d7a98dae3869fe0a60870d231a681979249be0218521719ddc7bc2d928747f101b9f0d3a5bc86b08b0a259a0b53'
