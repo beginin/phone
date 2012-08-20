38331a0fc5d8d14f79b0069a125d882fa10dbdb5
Vladimir Beginin <beginin@mail.ru>
2012-08-20 15:12:21 +0800

    Добавили меню сим карт в журнале

--- app/models/sim.rb
+++ app/models/sim.rb
@@ -1,4 +1,18 @@
 class Sim < ActiveRecord::Base
   attr_accessible :sirealnumber
   has_many :simnumlog
+  def self.freesims_id
+    simactive = Simnumlog.select(:sim_id).joins(:simperiod).where("'simperiods'.'dateout'  IS NULL ")
+    sa_id=Array.new
+    simactive.find_each do |d|
+      sa_id=sa_id+[d.sim_id]
+    end
+    simall = Sim.select(:id)
+    sl_id=Array.new
+    simall.find_each do |d|
+      sl_id = sl_id+[d.id]
+    end
+    sl_id
+    sl_id - sa_id
+  end
 end
