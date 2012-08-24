96c670056131445b9c0576c0d9c83e738b2b9029
Vladimir Beginin <beginin@mail.ru>
2012-08-24 15:14:29 +0800

    Добавление валидавции для Тарифа

--- app/models/sumlimit.rb
+++ app/models/sumlimit.rb
@@ -1,4 +1,7 @@
 class Sumlimit < ActiveRecord::Base
   attr_accessible :money, :namelimit
   has_many :userlog
+  validates :namelimit, :money, :presence => true
+  validates :money, :length => { :in => 1..5 }, :numericality => { :only_integer => true } 
+  validates :namelimit, :uniqueness => true
 end
