25563a2d41588fcb86c953cd656b7af7064c61d1
Vladimir Beginin <beginin@mail.ru>
2012-08-24 15:08:14 +0800

    Добавление валидации для Пользователя

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :firstname, :job, :midlename, :secondname
   has_many :userlog
+  validates :firstname, :secondname, :midlename , :presence => true
 end
