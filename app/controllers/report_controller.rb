7eb830207617cc5d8f243a17659dffb73992a9b2
Vladimir Beginin <beginin@mail.ru>
2012-09-10 12:13:16 +0800

    Добавление отчета

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,7 +1,9 @@
 class ReportController < ApplicationController
   def index
+  	@report1 = Load.find(:all , :order => "id DESC" )
   end
 
   def showload
+  	@report1 = Report.rep
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -24,19 +24,19 @@ class Report < ActiveRecord::Base
 			LEFT JOIN schedules ON (schedules.id = userlogs.schedule_id)
 			),
 			tnumbers_full AS (
-			SELECT tnumbers.* , tarifs.name
+			SELECT tnumbers.* , tarifs.name as tarifname
 			from tnumbers 
 			LEFT JOIN tarifs  ON (tnumbers.tarif_id = tarifs.id)
 			),
 			upr AS (
-			SELECT colllogs.tnumber, tnumbers_full.id as tnumberid ,userlogs_full.userlogid ,SUM(colllogs.coast)
+			SELECT colllogs.tnumber,tnumbers_full.tarifname , tnumbers_full.id as tnumberid ,userlogs_full.userlogid ,SUM(colllogs.coast)
 			FROM colllogs 
 			LEFT JOIN tnumbers_full ON (colllogs.tnumber = tnumbers_full.voicenumber)
 			LEFT JOIN simnumlogs_full ON (tnumbers_full.id = simnumlogs_full.tnumber_id AND colllogs.date >= simnumlogs_full.datein AND 
 			(colllogs.date <= simnumlogs_full.dateout OR simnumlogs_full.dateout IS NULL) )
 			LEFT JOIN userlogs_full ON (simnumlogs_full.sim_id = userlogs_full.sim_id AND colllogs.date >= userlogs_full.datein AND 
 			(colllogs.date <= userlogs_full.dateout OR userlogs_full.dateout IS NULL) )
-			GROUP BY colllogs.tnumber ,tnumberid , userlogid 
+			GROUP BY colllogs.tnumber,tnumbers_full.tarifname ,tnumberid , userlogid 
 			),
 			fin AS (
 			SELECT colllogs.tnumber, tnumbers_full.id,userlogs_full.user_id,SUM(colllogs.coast)
@@ -53,21 +53,22 @@ class Report < ActiveRecord::Base
 			GROUP BY colllogs.tnumber ,tnumbers_full.id , userlogs_full.user_id
 			)
 
-			SELECT upr.tnumber,upr.sum as uprsum,fin.sum as finsum , userlogs_full.secondname,userlogs_full.firstname,userlogs_full.midlename,
+			SELECT upr.tnumber,upr.tarifname,upr.sum as uprsum,fin.sum as sumfin , userlogs_full.secondname,userlogs_full.firstname,userlogs_full.midlename,
 			userlogs_full.namecfu,userlogs_full.money,userlogs_full.datein,userlogs_full.dateout,userlogs_full.timein,
 			userlogs_full.timeout
 			FROM upr
 			LEFT JOIN fin ON (fin.tnumber = upr.tnumber)
 			LEFT JOIN userlogs_full ON (upr.userlogid = userlogs_full.userlogid)
 			"
-		self.find_by_sql(sql)
+		#self.find_by_sql(sql)
+		ActiveRecord::Base.connection.select_all( sql )
  		
  	end
  
   	# Manually define the columns used by this model
   	column :tnumber, :integer
   	column :uprsum, :deciaml
-  	column :finsim, :deciaml
+  	column :finsum, :deciaml
   	column :secondname, :string
  	column :firstname, :string
  	column :midlename, :string
